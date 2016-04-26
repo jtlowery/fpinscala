a52700750ad7242d9e6f488a837502d474dae847
Joel Lowery <joeltlowery@gmail.com>
2016-04-26 02:35:09 -0500

    Completed some of chap3 exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -23,6 +23,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  // exercise 3.1 should be 3
   val x = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
@@ -49,20 +50,87 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  // exercise 3.2
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, t) => t
+    }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  // exercise 3.3
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => sys.error("setting head of empty list")
+      case Cons(_, t) => Cons(h, t)
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  // exercise 3.4
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => sys.error("dropping elements of empty list")
+      case Cons(_, t) => drop(t, n - 1)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  // exercise 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => sys.error("dropping elements of empty list")
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  // exercise 3.6
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("dropping tail of empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  // exercise 3.7
+  // can product implemented using foldRight immediately halt the recursion
+  // and return 0.0 if it encounters a 0.0
+  // A: This isn't possible because foldRight must traverse the list to the end
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  // exercise 3.8
+  // see what happens when you call Nil and Cons themselves to foldRight
+  // foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_))
+  // what do you think this says about the relationship between
+  // foldRight and the data constructors of the List?
+  // A: Get back the original list.
+  // foldRight replaces 'Nil' constructor of the list with the 'z' argument
+  // which is 'Nil' in this case and replaces the 'Cons' constructor
+  // replaces the 'Cons' constructor with 'f' which is 'Cons' here
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  // exercise 3.9
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, y) => 1 + y)
+
+  // exercise 3.10
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+
+  // exercise 3.11 sum in terms of foldLeft
+  def sumFoldLeft(l: List[Int]) =
+    foldLeft(l, 0)((x, y) => x + y)
+
+  // exercise 3.11 product in terms of foldLeft
+  def productFoldLeft(l: List[Double]) =
+    foldLeft(l, 1.0)((x, y) => x * y)
+
+  // exercise 3.11 length in terms of foldLeft
+  def lengthFoldLeft[A](l: List[A]) =
+    foldLeft(l, 0)((x, _) => x + 1)
+
+  // exercise 3.12 reverse an input list
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((h, t) => Cons(t, h))
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+
 }
